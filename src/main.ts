85852e2e360fe6f6560ade095e7c2afc36e1b509
ega4432 <38056766+ega4432@users.noreply.github.com>
2022-01-16 14:58:09 +0900

    bug: fixed ts type of google apps script

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
+type Headers = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any
+}
+
 const booklogId: string = PropertiesService.getScriptProperties()
-  .getProperty('BOOKLOG_ID')
+  .getProperty('BOOKLOG_ID') || ""
 
 const booklogPassword: string = PropertiesService.getScriptProperties()
-  .getProperty('BOOKLOG_PASSWORD')
+  .getProperty('BOOKLOG_PASSWORD') || ""
 
-const logSheet: Sheet = SpreadsheetApp.getActiveSpreadsheet()
+const logSheet = SpreadsheetApp.getActiveSpreadsheet()
   .getSheetByName('log')
 
-const searchEmail = (): GmailThread[] => {
+const searchEmail = () => {
   return GmailApp.search("from:digital-no-reply@amazon.co.jp in:inbox")
 }
 
 const loginToBooklog = (): string[] => {
   const url = 'https://booklog.jp/login'
-  const res = UrlFetchApp.fetch(url, {
+  const params = {
     method: 'post',
     followRedirects: false,
     headers: { Referer: url },
@@ -21,29 +26,35 @@ const loginToBooklog = (): string[] => {
       account: booklogId,
       password: booklogPassword
     }
-  })
+  } as GoogleAppsScript.URL_Fetch.URLFetchRequestOptions
+  const res = UrlFetchApp.fetch(url, params)
 
-  const headers = res.getAllHeaders()
+  const headers = res.getAllHeaders() as Headers
 
-  if (typeof headers['Set-Cookie'] === 'undefined') {
-    return []
-  }
-
-  const cookies = headers['Set-Cookie'] === 'string'
+  if ('Set-Cookie' in headers) {
+    const cookies = headers['Set-Cookie'] === 'string'
       ? [headers['Set-Cookie']]
       : headers['Set-Cookie']
 
-  for (let i = 0; i < cookies.length; i++) {
-    cookies[i] = cookies[i].split(';')[0]
-  }
+    for (let i = 0; i < cookies.length; i++) {
+      cookies[i] = cookies[i].split(';')[0]
+    }
 
-  return cookies
+    return cookies
+    return []
+  } else {
+    return []
+  }
 }
 
-const getAsinList = (thread: GmailThread): string[] => {
+const getAsinList = (thread: GoogleAppsScript.Gmail.GmailThread): string[] => {
   const asinList = thread.getMessages()[0]
     .getBody()
     .match(/dp%2F.{10}/g)
+
+  if (!asinList) {
+    return []
+  }
 
   for (let i = 0; i < asinList.length; i++) {
     asinList[i] = asinList[i].slice(-10)
@@ -52,7 +63,7 @@ const getAsinList = (thread: GmailThread): string[] => {
   return asinList
 }
 
-const uploadBook = (cookies: string[], asinList: string[]): HTTPResponse => {
+const uploadBook = (cookies: string[], asinList: string[]): GoogleAppsScript.URL_Fetch.HTTPResponse => {
   const url = 'https://booklog.jp/input'
 
   return UrlFetchApp.fetch(url, {
@@ -70,7 +81,9 @@ const uploadBook = (cookies: string[], asinList: string[]): HTTPResponse => {
 }
 
 const log = (asin: string, text: string): void => {
-  logSheet.appendRow([new Date(), asin, text])
+  if (logSheet && text) {
+    logSheet.appendRow([new Date(), asin, text])
+  }
 }
 
 export const main = (): void => {
@@ -88,7 +101,7 @@ export const main = (): void => {
     return
   }
 
-  threads.forEach((thread: GmailThread) => {
+  threads.forEach((thread: GoogleAppsScript.Gmail.GmailThread): void => {
     const asinList = getAsinList(thread)
     if (typeof asinList !== 'undefined') {
       Logger.log('asin is: ' + asinList.join(', '))
@@ -97,9 +110,14 @@ export const main = (): void => {
       const results = res.getContentText()
         .match(/.*tc(?:pink|blue) t10M.*/g)
 
-      results.forEach((result: string) => {
-        log(asinList.join(', '), result.match(/>(.*)</)[1])
-      })
+      if (results) {
+        results.forEach((result: string) => {
+          const text = result.match(/>(.*)</)
+          if (text) {
+            log(asinList.join(', '), text[1])
+          }
+        })
+      }
 
       thread.markRead().moveToArchive()
     } else {
